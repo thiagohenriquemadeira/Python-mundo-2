a4203d15707657a0f985fab034de58903d384aff
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-19 11:48:30 -0300

    Update desafio37.py

---
+++ desafio37.py
@@ -0,0 +1,8 @@
+numero1 = str(input('Escreva um número inteiro? '))
+print(numero1)
+contador = 0
+final = len(numero1) 
+print(final)
+while contador < final :
+    print(contador)
+    contador += 1
