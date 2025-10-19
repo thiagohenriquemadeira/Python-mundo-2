1361707470822b23b5687da2b93d6f375652a68c
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-19 17:56:45 -0300

    Create desafio39.py

---
+++ desafio39.py
@@ -0,0 +1,7 @@
+idade = int(input('Digite sua idade?'))
+if idade < 18:
+    print('Você vai ter que ser alistar quando você tiver 18 anos!')
+elif idade == 18:
+    print('Chegou a hora de se alistar!')
+else:
+    print('Já passou o tempo de sé alistar!')
