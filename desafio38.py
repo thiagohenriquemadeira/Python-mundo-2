8616951f7376d6b2c4bfac24c517b1f8a3483c1e
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-19 17:47:53 -0300

    Create desafio38.py

---
+++ desafio38.py
@@ -0,0 +1,8 @@
+numero1 = int(input('Digite um número? '))
+numero2 = int(input('Digite outo número? '))
+if numero1 > numero2:
+    print('O primeiro número e o maior')
+elif numero1 < numero2:
+    print('O segundo número e maior')
+else:
+    print('Os números são iguais')
