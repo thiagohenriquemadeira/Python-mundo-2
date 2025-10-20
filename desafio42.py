43ba8be57a7dbfa074a97b7adc289c13ffd1a80b
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-20 19:13:33 -0300

    Create desafio42.py

---
+++ desafio42.py
@@ -0,0 +1,22 @@
+print('-='*20)
+print('Analizador de Triângulo')
+print('-='*20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR UM TRIÂNGULO!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR UM TRIÂNGULO!')
+if r1 == r2 and r2 == r3:
+    print('Os segmentos acima PODEM FORMAR UM EQUILÁTERO!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR UM EQUILÁTERO')
+if r1 == r2 or r1 == r3 or r2 == r3:
+    print('Os segmentos acima PODEM FORMAR UM ISÓSCELES')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR UM ISÓSCELES')
+if r1 != r2 and r1 != r3 and r2 != r3:
+    print('Os segmentos acima PODEM FORMAR UM ESCALENO')
+else:
+    print('s segmentos acima NÃO PODEM FORMAR UM ESCALENO')
