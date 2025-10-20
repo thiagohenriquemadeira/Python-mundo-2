61d2933697ddd29546b4e1f292264c120ff79915
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-20 10:26:08 -0300

    Update desafio39.py

--- desafio39.py
+++ desafio39.py
@@ -1,6 +1,14 @@
 idade = int(input('Digite sua idade?'))
+contador = 0
+idademenor = idade
 if idade < 18:
-    print('Você vai ter que ser alistar quando você tiver 18 anos!')
+    while idademenor < 18:
+        idademenor += 1
+    idademenor = idademenor - idade
+    if idademenor == 1:
+        print('Você vai ter que ser alistar quando você tiver 18 anos! você tem {} anos e faltam {} ano para se alistar.'.format(idade, idademenor))
+    else:
+        print('Você vai ter que ser alistar quando você tiver 18 anos! você tem {} anos e faltam {} anos para se alistar.'.format(idade, idademenor))
 elif idade == 18:
     print('Chegou a hora de se alistar!')
 else:
