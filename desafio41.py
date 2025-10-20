e4e7a8656e5e4764889fcedbc2241cedfc38ac37
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-20 15:21:12 -0300

    Create desafio41.py

---
+++ desafio41.py
@@ -0,0 +1,11 @@
+idade =int(input('Qual a sua idade? '))
+if idade <= 9:
+    print(' Você tem {} anos e um atleta MIRIM'.format(idade))  
+elif idade <= 14 and idade  > 9:
+    print('Você tem {} anos e um atleta INFANTIL'.format(idade))
+elif idade <= 19 and idade > 14:
+    print('Você tem {} anos e um atleta JUNIOR'.format(idade))
+elif idade <= 20 and idade > 19:
+    print('Você tem {} anos e um atleta SÊNIOR'.format(idade))
+else:
+    print('Você tem {} anos e um atleta MASTER'.format(idade))
