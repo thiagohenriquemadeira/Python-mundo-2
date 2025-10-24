07a85c0706ffcc890b249fa4aed42349f9b0bb44
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-24 14:58:35 -0300

    Create desafio50.py

---
+++ desafio50.py
@@ -0,0 +1,10 @@
+c = 0
+c2 = 6
+n = []
+for c in range(c, 6, 1):
+    n.append(input('Digite um número inteiro?'))
+print(n)
+print(n[0])
+c = 0
+for c in range(c, 6, 1):
+    n.append(input('Digite um número inteiro?'))
