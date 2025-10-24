4aa482b5ef666f399892585f141b41c5c05b7d07
thiagohenriquemadeira <thiagohenriquemadeiranunes5@gmail.com>
2025-10-23 21:16:44 -0300

    Update desafio45.py

--- desafio45.py
+++ desafio45.py
@@ -10,7 +10,7 @@ escolhaint = escolha.isdigit()
 if escolha == '1' or escolha == '2' or escolha == '3':
     escolha = int(escolha)
 else:
-    while escolhaletra == True and escolhaint == False:
+    while True:
         print('=-'*30)
         print('Tente novamente só pode ser 1,2 ou 3')
         print('=-'*30)
@@ -20,8 +20,32 @@ else:
         escolha = input('Digite o número que representa sua escolha? ')
         escolhaletra = escolha.isalpha()
         escolhaint = escolha.isdigit()
+        if escolha == '1' or escolha == '2' or escolha == '3':
+            break
     escolha = int(escolha)
 numeroescolhido = random.randint(1, 3)
+maquinaescolheu = ''
+voceescolheu = ''
+print('=-'*20)
 if numeroescolhido == 1:
     print('O algoritimo escolheu tesoura!')
-
+    maquinaescolheu = 'TESOURA'
+elif numeroescolhido == 2:
+    print('O algoritimo escolheu papel!')
+    maquinaescolheu = 'PAPEL'
+elif numeroescolhido ==  3:
+    print('O algoritimo escreveu pedra')
+    maquinaescolheu = 'PEDRA'
+if escolha == 1:
+    voceescolheu = 'TESOURA'
+elif escolha == 2:
+    voceescolheu = 'PAPEL'
+elif escolha == 3:
+    voceescolheu = 'PEDRA'
+print('=-'*20)
+if numeroescolhido == escolha:
+    print('Os dois escolheram {} deu Empate!!!'.format(voceescolheu))
+elif (numeroescolhido == 1 and escolha == 2) or (numeroescolhido == 2 and escolha == 3) or (numeroescolhido == 3 and escolha == 1):
+    print('Você escolheu {} e maquina {} maquina sempre vençe!'.format(voceescolheu, maquinaescolheu))
+elif (numeroescolhido == 2 and escolha == 1) or (numeroescolhido == 3 and escolha == 2) or (numeroescolhido == 1 and escolha == 3):
+    print('Você escolheu {} e a maquina {}, Então você venceu a maquina!!!'.format(voceescolheu, maquinaescolheu))
